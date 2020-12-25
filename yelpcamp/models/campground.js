97dee94976f3acac9d25e803b8e817b8b8e3036d
qc <qchen125@gmail.com>
2020-12-25 18:32:19 -0500

    completed middleware

--- yelpcamp/models/campground.js
+++ yelpcamp/models/campground.js
@@ -3,16 +3,10 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String
+});
 
-})
-
-
-
-
-
-
-
-module.exports = mongoose.model('Campground', CampgroundSchema)
+module.exports = mongoose.model('Campground', CampgroundSchema);
